a6c8113d50e7f31dbba6306fe27b80ad5fa77805
William Woodruff <william@yossarian.net>
2023-05-01 22:21:41 -0400

    main: dump signatures manually

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ use chrono::{DateTime, Utc};
 use clap::Parser;
 use sequoia_openpgp::{
     cert::prelude::KeyAmalgamation,
-    crypto::mpi::{PublicKey, MPI},
-    packet::key::{PrimaryRole, PublicParts, SubordinateRole},
+    crypto::mpi::{PublicKey, Signature as SignatureParams, MPI},
+    packet::{
+        key::{PrimaryRole, PublicParts, SubordinateRole},
+        Signature,
+    },
     parse::Parse,
-    types::RevocationKey,
+    types::KeyFlags,
     Cert,
 };
 use serde::Serialize;
@@ -121,12 +124,109 @@ impl From<&PublicKey> for DumpableKeyParams {
 }
 
 #[derive(Serialize)]
-struct KeyPurposes {
+struct DumpableKeyFlags {
     authentication: bool,
     certification: bool,
     signing: bool,
     storage_encryption: bool,
     transport_encryption: bool,
+}
+
+impl From<KeyFlags> for DumpableKeyFlags {
+    fn from(flags: KeyFlags) -> Self {
+        Self {
+            authentication: flags.for_authentication(),
+            certification: flags.for_certification(),
+            signing: flags.for_signing(),
+            storage_encryption: flags.for_storage_encryption(),
+            transport_encryption: flags.for_transport_encryption(),
+        }
+    }
+}
+
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Serialize)]
+#[serde(tag = "algorithm")]
+enum DumpableSignatureParams {
+    RSA { s: DumpableMPI },
+    DSA { r: DumpableMPI, s: DumpableMPI },
+    ElGamal { r: DumpableMPI, s: DumpableMPI },
+    EdDSA { r: DumpableMPI, s: DumpableMPI },
+    ECDSA { r: DumpableMPI, s: DumpableMPI },
+    Unknown,
+}
+
+impl From<&SignatureParams> for DumpableSignatureParams {
+    fn from(params: &SignatureParams) -> Self {
+        match params {
+            SignatureParams::RSA { s } => Self::RSA { s: s.into() },
+            SignatureParams::DSA { r, s } => Self::DSA {
+                r: r.into(),
+                s: s.into(),
+            },
+            SignatureParams::ElGamal { r, s } => Self::ElGamal {
+                r: r.into(),
+                s: s.into(),
+            },
+            SignatureParams::EdDSA { r, s } => Self::EdDSA {
+                r: r.into(),
+                s: s.into(),
+            },
+            SignatureParams::ECDSA { r, s } => Self::ECDSA {
+                r: r.into(),
+                s: s.into(),
+            },
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct DumpableSignature {
+    version: u8,
+    algorithm: String,
+    hash_algorithm: String,
+    signature_params: DumpableSignatureParams,
+    digest_prefix: String,
+    level: usize,
+    exportable: bool,
+    #[serde(rename = "type")]
+    typ: String,
+    creation: Option<String>,
+    expiration: Option<String>,
+    key_validity_period: Option<f64>,
+    key_flags: Option<DumpableKeyFlags>,
+    issuer_key_ids: Vec<String>,
+    issuer_fingerprints: Vec<String>,
+    embedded_signatures: Vec<DumpableSignature>,
+    intended_recipients: Vec<String>,
+}
+
+impl From<&Signature> for DumpableSignature {
+    fn from(sig: &Signature) -> Self {
+        Self {
+            version: sig.version(),
+            algorithm: sig.pk_algo().to_string(),
+            hash_algorithm: sig.hash_algo().to_string(),
+            signature_params: sig.mpis().into(),
+            digest_prefix: hex::encode(sig.digest_prefix()),
+            level: sig.level(),
+            exportable: sig.exportable().is_ok(),
+            typ: sig.typ().to_string(),
+            creation: sig
+                .signature_creation_time()
+                .map(|t| DateTime::<Utc>::from(t).to_rfc3339()),
+            expiration: sig
+                .signature_expiration_time()
+                .map(|t| DateTime::<Utc>::from(t).to_rfc3339()),
+            key_validity_period: sig.key_validity_period().map(|d| d.as_secs_f64()),
+            key_flags: sig.key_flags().map(Into::into),
+            issuer_key_ids: sig.issuers().map(|kid| kid.to_hex()).collect(),
+            issuer_fingerprints: sig.issuer_fingerprints().map(|fp| fp.to_hex()).collect(),
+            embedded_signatures: sig.embedded_signatures().map(Into::into).collect(),
+            intended_recipients: sig.intended_recipients().map(|ir| ir.to_hex()).collect(),
+        }
+    }
 }
 
 #[derive(Serialize)]
@@ -135,76 +235,44 @@ struct DumpableKey {
     parameters: DumpableKeyParams,
     fingerprint: String,
     keyid: String,
-    // expiration: Option<String>,
     creation: String,
-    // purposes: KeyPurposes,
+    self_signatures: Vec<DumpableSignature>,
+    attestations: Vec<DumpableSignature>,
+    certifications: Vec<DumpableSignature>,
+    self_revocations: Vec<DumpableSignature>,
+    other_revocations: Vec<DumpableSignature>,
 }
 
 impl From<KeyAmalgamation<'_, PublicParts, PrimaryRole, ()>> for DumpableKey {
     fn from(key: KeyAmalgamation<'_, PublicParts, PrimaryRole, ()>) -> Self {
-        // let expiration = key
-        //     .key_expiration_time()
-        //     .map(|t| DateTime::<Utc>::from(t).to_rfc3339());
-        let creation = DateTime::<Utc>::from(key.creation_time()).to_rfc3339();
-        // let purposes = KeyPurposes {
-        //     authentication: key.for_authentication(),
-        //     certification: key.for_certification(),
-        //     signing: key.for_signing(),
-        //     storage_encryption: key.for_storage_encryption(),
-        //     transport_encryption: key.for_transport_encryption(),
-        // };
-        let key = key.key();
-
         Self {
             algorithm: key.pk_algo().to_string(),
             parameters: key.mpis().into(),
             fingerprint: key.fingerprint().to_hex(),
             keyid: key.keyid().to_hex(),
-            // expiration,
-            creation,
-            // purposes,
+            creation: DateTime::<Utc>::from(key.creation_time()).to_rfc3339(),
+            self_signatures: key.self_signatures().map(Into::into).collect(),
+            attestations: key.attestations().map(Into::into).collect(),
+            certifications: key.certifications().map(Into::into).collect(),
+            self_revocations: key.self_revocations().map(Into::into).collect(),
+            other_revocations: key.other_revocations().map(Into::into).collect(),
         }
     }
 }
 
 impl From<KeyAmalgamation<'_, PublicParts, SubordinateRole, ()>> for DumpableKey {
     fn from(key: KeyAmalgamation<'_, PublicParts, SubordinateRole, ()>) -> Self {
-        // let expiration = key
-        //     .key_expiration_time()
-        //     .map(|t| DateTime::<Utc>::from(t).to_rfc3339());
-        let creation = DateTime::<Utc>::from(key.creation_time()).to_rfc3339();
-        // let purposes = KeyPurposes {
-        //     authentication: key.for_authentication(),
-        //     certification: key.for_certification(),
-        //     signing: key.for_signing(),
-        //     storage_encryption: key.for_storage_encryption(),
-        //     transport_encryption: key.for_transport_encryption(),
-        // };
-        let key = key.key();
-
         Self {
             algorithm: key.pk_algo().to_string(),
             parameters: key.mpis().into(),
             fingerprint: key.fingerprint().to_hex(),
             keyid: key.keyid().to_hex(),
-            // expiration,
-            creation,
-            // purposes,
-        }
-    }
-}
-
-#[derive(Serialize)]
-struct DumpableRevocationKey {
-    algorithm: String,
-    fingerprint: String,
-}
-
-impl From<&RevocationKey> for DumpableRevocationKey {
-    fn from(key: &RevocationKey) -> Self {
-        Self {
-            algorithm: key.revoker().0.to_string(),
-            fingerprint: key.revoker().1.to_hex(),
+            creation: DateTime::<Utc>::from(key.creation_time()).to_rfc3339(),
+            self_signatures: key.self_signatures().map(Into::into).collect(),
+            attestations: key.attestations().map(Into::into).collect(),
+            certifications: key.certifications().map(Into::into).collect(),
+            self_revocations: key.self_revocations().map(Into::into).collect(),
+            other_revocations: key.other_revocations().map(Into::into).collect(),
         }
     }
 }
@@ -217,8 +285,6 @@ struct DumpableCert {
     userids: Vec<String>,
     primary_key: DumpableKey,
     subkeys: Vec<DumpableKey>,
-    // revocation_keys: Vec<DumpableRevocationKey>,
-    // alive: bool,
 }
 
 impl From<Cert> for DumpableCert {
@@ -233,11 +299,6 @@ impl From<Cert> for DumpableCert {
                 .collect(),
             primary_key: cert.primary_key().into(),
             subkeys: cert.keys().subkeys().map(DumpableKey::from).collect(),
-            // revocation_keys: cert
-            //     .revocation_keys()
-            //     .map(DumpableRevocationKey::from)
-            //     .collect(),
-            // alive: cert.alive().is_ok(),
         }
     }
 }
